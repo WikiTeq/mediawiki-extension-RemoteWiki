f7b78670a500c10e2b71a63927615a2e303388f5
Daniel Scherzer <daniel@wikiteq.com>
2023-06-02 16:28:07 +0200

    Start adding tests
    Create DummyTest to just assert that `true` is true, and set up GitHub actions
    to automatically run the tests.

---
+++ tests/phpunit/integration/DummyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MediaWiki\Extension\RemoteWiki\Tests\Integration;
+
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @coversNothing Just a sample
+ * @group extension-RemoteWiki
+ */
+class DummyTest extends MediaWikiIntegrationTestCase {
+
+    public function testPass() {
+        $this->assertTrue( true );
+    }
+
+}
